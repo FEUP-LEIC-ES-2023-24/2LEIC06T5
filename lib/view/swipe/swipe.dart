5374ecf50831a9b28b63df81f1786783726d9156
DGoiana <diogogoianam@gmail.com>
2024-03-25 11:36:28 +0000

    Refactor

--- lib/view/swipe/swipe.dart
+++ lib/view/swipe/swipe.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pagepal/view/swipe/widgets/swiper.dart';
 import 'package:pagepal/view/templates/general/general_page.dart';
-
-import '../../model/slide_card.dart';
 
 class SwipePageView extends StatefulWidget {
   const SwipePageView({super.key});
@@ -13,6 +12,65 @@ class SwipePageView extends StatefulWidget {
 class SwipePageViewState extends GeneralPageState {
   @override
   Widget getBody(BuildContext context) {
-    return SlideCard();
+    return Column(
+      children: [
+        buildHeader(),
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Swiper(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildHeader() {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(right: 10, top: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(elevation: 10),
+                child: const Row(
+                  children: [
+                    Text(
+                      "Location ",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(
+                      Icons.add_circle,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 85,
+          child: Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Discover",
+                      style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFCCD5AE))),
+                  Row(children: [
+                    Icon(Icons.add),
+                    // TODO: THIS IS HARDCODED
+                    Text("Porto, Portugal")
+                  ])
+                ],
+              )),
+        )
+      ],
+    );
   }
 }
