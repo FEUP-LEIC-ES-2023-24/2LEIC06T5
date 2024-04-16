17fcb04cbd5930dd28034b62cba2830171d6e345
Alexandre Silva <asilva1604@outlook.com>
2024-04-16 16:45:51 +0100

    added search bar and text in chat view

--- lib/view/chat/chat.dart
+++ lib/view/chat/chat.dart
@@ -11,13 +11,56 @@ class ChatPageView extends StatefulWidget {
 class ChatPageViewState extends GeneralPageState {
   @override
   Widget getBody(BuildContext context) {
-    return const SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("CHAT PAGE")],
-        ));
+    return Column(
+      children: [
+        buildHeader()
+      ],
+    );
   }
+
+  Widget buildHeader() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 25, top: 55, right: 25),
+          child: Text(
+            "Messages",
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFCCD5AE),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[300], // Grey background color
+            borderRadius: BorderRadius.circular(20), // Rounded corners
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 10), // Reduced padding
+          margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10), // Margin to adjust size
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.search), // Search icon
+              SizedBox(width: 10), // Spacer between icon and TextField
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search...",
+                    border: InputBorder.none, // Remove border around TextField
+                  ),
+                  onChanged: (value) {
+                    // Implement search functionality
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
 }
