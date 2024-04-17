76863b7143781299a21e5d4ba139f83115dd9b21
DGoiana <diogogoianam@gmail.com>
2024-04-17 11:07:30 +0100

    User authentication

---
+++ lib/model/user.dart
@@ -0,0 +1,31 @@
+import 'package:pagepal/model/book.dart';
+
+class User {
+  User(
+      {required this.email,
+      required this.username,
+      required this.likedGenres,
+      required this.books});
+  final String email;
+  final String username;
+  final List<String> likedGenres;
+  //final Location location;
+  final List<Book> books;
+
+  User.fromJson(Map<String, Object?> json)
+      : this(
+          email: json['email']! as String,
+          username: json['username']! as String,
+          likedGenres: json['likedGenre']! as List<String>,
+          books: json['owns']! as List<Book>,
+        );
+
+  Map<String, Object?> toJson() {
+    return {
+      'email': email,
+      'username': username,
+      'likedGenre': likedGenres,
+      'owns': books,
+    };
+  }
+}
