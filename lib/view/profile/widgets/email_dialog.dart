6d28f77ed1bb3c7c6d982c137aebfda7769d437b
DGoiana <diogogoianam@gmail.com>
2024-04-23 20:11:16 +0100

    Lint fix...again

--- lib/view/profile/widgets/email_dialog.dart
+++ lib/view/profile/widgets/email_dialog.dart
@@ -1,7 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:pagepal/controller/queries.dart';
 
 class ChangeEmailDialog extends StatefulWidget {
   const ChangeEmailDialog({super.key});
