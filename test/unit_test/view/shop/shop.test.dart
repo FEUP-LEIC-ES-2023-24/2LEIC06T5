4dd06351c5dacba2337ff7616660458176fa3e53
rubuy-74 <rubemviscard2635@gmail.com>
2024-05-06 01:41:14 +0100

    First Batch of Tests

---
+++ test/unit_test/view/shop/shop.test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pagepal/view/shop/shop.dart';
+
+void main() {
+  testWidgets('ShopPageView Widget Test', (widgetTester) async {
+    await widgetTester.pumpWidget(const MaterialApp(
+      home:ShopPageView(),
+    ));
+    expect(find.text('SHOP PAGE'), findsOneWidget);
+  });
+}
+
